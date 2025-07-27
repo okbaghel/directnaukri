17d45d986193b516905eb5efa13931ed028d872c
Yogesh Baghel <yogeshbaghel09.in@gmail.com>
2025-07-27 14:34:48 +0530

    fix

--- src/app/api/auth/google-callback/route.js
+++ src/app/api/auth/google-callback/route.js
@@ -22,7 +22,8 @@ const origin = `${url.protocol}//${url.host}`;
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${origin}/api/auth/google-callback` // Dynamic!
+  // `${origin}/api/auth/google-callback` // Dynamic!
+  process.env.GOOGLE_REDIRECT_URI,
 );
 
 
